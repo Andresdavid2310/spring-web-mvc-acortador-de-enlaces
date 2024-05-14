f781bef390c8d22c69ad4b987980992595f8f0eb
Andres Benavides <dandavi2310@hotmail.com>
2024-05-14 03:43:34 -0500

    creacion del proyecto spring

---
+++ src/main/java/com/andresbenavides/springwebmvcacortadordeenlaces/SpringWebMvcAcortadorDeEnlacesApplication.java
@@ -0,0 +1,13 @@
+package com.andresbenavides.springwebmvcacortadordeenlaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcAcortadorDeEnlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcAcortadorDeEnlacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andresbenavides/springwebmvcacortadordeenlaces/SpringWebMvcAcortadorDeEnlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresbenavides.springwebmvcacortadordeenlaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMvcAcortadorDeEnlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
